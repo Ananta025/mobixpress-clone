8a16ce103d8ba31b7472ec1c3b941ad214b9c58b
Ananta Chandra Das <danantach@gmail.com>
2025-07-15 23:28:48 +0530

    add some feature

---
+++ src/app/services/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold mb-6">Our Services</h1>
+      <p className="mb-4">
+        We offer a wide range of services to meet your needs.
+      </p>
+      <ul className="list-disc pl-6 mb-4">
+        <li>Consulting</li>
+        <li>Development</li>
+        <li>Design</li>
+        <li>Support</li>
+      </ul>
+    </div>
+  )
+}
